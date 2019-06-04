e521dbb883fd475fdcca0c0046af027749a265c7
wiktor <wiktor.nizio@gmail.com>
2019-06-04 08:48:38 +0200

    Removed obsolete code.

--- compass/src/main/java/pl/org/seva/compass/main/extension/LiveData.kt
+++ compass/src/main/java/pl/org/seva/compass/main/extension/LiveData.kt
@@ -21,11 +21,6 @@ package pl.org.seva.compass.main.extension
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 
 operator fun <T> LiveData<T>.invoke(owner: LifecycleOwner, observer: (T) -> Unit) =
         observe(owner) { observer(it) }
-
-@Suppress("unused")
-fun <T> LiveData<T>.observe(owner: LifecycleOwner, f: (T) -> Unit) =
-        observe(owner, Observer<T> { f(it) })
